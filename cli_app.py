34b5a74d1dee6691eb4322cbf8b55bada9c4e5e1
dinesh-rengasamy <dinesh.rengasamy@freshworks.com>
2021-09-09 18:56:37 +0530

    v1 of search-cli

---
+++ cli_app.py
@@ -0,0 +1,117 @@
+from cmd import Cmd
+from glob import glob
+from collections import defaultdict
+import os
+
+SEARCH_FOLDER = "./search_folder/" 
+
+class MyPrompt(Cmd):
+    prompt = 'search-cli> '
+    intro = "Welcome to the search-cli. Enter 'help' to explore options."
+ 
+    def do_index(self, inp):
+        if inp:
+            print("Indexing does not require any parameters")
+        print("Indexing files in ./search_folder...")
+        self.inverted_index = index_files()
+
+    def help_index(self):
+        print("Indexes documents in ./search_folder")
+
+    def do_search(self, search_txt):
+        if not search_txt:
+            print("Please enter some search text")
+            return
+        if not self.inverted_index:
+            print("Please index the files first, before searching. Enter 'index'")
+            return
+        search_files(search_txt, self.inverted_index)
+        # return True
+
+    def help_search(self):
+        print("Searches files in ./search_folder for words in search_txt and ranks top 10")    
+
+    def do_exit(self, inp):
+        print("Bye")
+        return True
+
+    def help_exit(self):
+        print("Enter 'exit' to exit the cli")
+ 
+    # def do_add(self, inp):
+    #     print("adding '{}'".format(inp))
+ 
+    # def help_add(self):
+    #     print("Add a new entry to the system.")
+ 
+    # def default(self, inp):
+    #     if inp == 'x' or inp == 'q':
+    #         return self.do_exit(inp)
+ 
+    #     print("Default: {}".format(inp))
+ 
+    # do_EOF = do_exit
+    # help_EOF = help_exit
+ 
+ 
+def index_files():
+    
+    files = glob(f"{SEARCH_FOLDER}*")
+    
+    # print(files)
+    inverted_index = defaultdict(lambda: defaultdict(lambda: 0))
+    
+    for file in files:
+        
+        words = []
+        with open(file, "r") as file_reader:
+            words = file_reader.read().split()
+        
+        for word in words:
+            inverted_index[word][os.path.basename(file)] += 1
+        
+    for key in list(inverted_index.keys())[:3]:
+        print(inverted_index[key])
+
+    return inverted_index
+            
+def search_files(search_txt: str, inverted_index: defaultdict(lambda: defaultdict(lambda: 0))):
+    """
+    Searches files in SEARCH_FOLDER for search_txt
+
+    Args:
+    search_txt - Text to be searched
+    inverted_index - Indexed word dict represeting words
+                     present in each file
+
+    Returns:
+    None
+    """
+    # no. of words present in each file, init to 0
+    search_words_in_files =  defaultdict(lambda: 0)
+
+    # words to be searched
+    search_words = search_txt.split()
+    total_words = len(search_words)
+
+    # searching all words in all files
+    #     using inverted index
+    files = glob(f"{SEARCH_FOLDER}*")
+    for word in search_words:
+        for file in files:
+            filename = os.path.basename(file)
+            if inverted_index[word][filename]:
+                    search_words_in_files[filename] += 1
+
+    # converting to perc
+    for filename in search_words_in_files:
+        search_words_in_files[filename] = round(search_words_in_files[filename]/total_words * 100,2)
+
+    # final files rank sorted by scores(0 to 100 based perc of no. of words)
+    files_ranked = sorted(search_words_in_files.items(), key=lambda x:x[1], reverse=True)
+
+    # printing output files by rank
+    print(files_ranked[:10])
+
+if __name__ == '__main__':
+    MyPrompt().cmdloop()
